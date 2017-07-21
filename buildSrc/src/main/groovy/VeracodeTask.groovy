fc977c975a243467a3f048c2ca04600fc2d66502
David Gamba <david.gamba@calgaryscientific.com>
2017-07-21 13:48:10 -0600

    rmd-20676 Allow use of API credentials
    This allows to provide either username and password or key and ID.

--- buildSrc/src/main/groovy/VeracodeTask.groovy
+++ buildSrc/src/main/groovy/VeracodeTask.groovy
@@ -1,3 +1,29 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2017 Calgary Scientific Incorporated
+ *
+ * Copyright (c) 2013-2014 kctang
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
@@ -68,15 +94,23 @@ abstract class VeracodeTask extends DefaultTask {
     }
 
     protected UploadAPIWrapper loginUpdate() {
-        UploadAPIWrapper update = new UploadAPIWrapper()
-        update.setUpCredentials(veracodeUser.username, veracodeUser.password)
-        return update
+        UploadAPIWrapper api = new UploadAPIWrapper()
+        if (veracodeUser.apiCredentials) {
+            api.setUpApiCredentials(veracodeUser.id, veracodeUser.key)
+        } else {
+            api.setUpCredentials(veracodeUser.username, veracodeUser.password)
+        }
+        return api
     }
 
     protected ResultsAPIWrapper loginResults() {
-        ResultsAPIWrapper results = new ResultsAPIWrapper()
-        results.setUpCredentials(veracodeUser.username, veracodeUser.password)
-        return results
+        ResultsAPIWrapper api = new ResultsAPIWrapper()
+        if (veracodeUser.apiCredentials) {
+            api.setUpApiCredentials(veracodeUser.id, veracodeUser.key)
+        } else {
+            api.setUpCredentials(veracodeUser.username, veracodeUser.password)
+        }
+        return api
     }
 
     protected Node writeXml(String filename, String content) {
@@ -100,7 +134,10 @@ abstract class VeracodeTask extends DefaultTask {
     }
 
     static class VeracodeUser {
+        boolean apiCredentials
         String username
         String password
+        String id
+        String key
     }
 }
